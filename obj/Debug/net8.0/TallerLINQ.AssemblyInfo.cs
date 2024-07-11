28d255a13c41d9862a9128669349a8f6efedd2de
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-07-11 08:38:47 -0500

    creacion de carpetas y archivos

--- obj/Debug/net8.0/TallerLINQ.AssemblyInfo.cs
+++ obj/Debug/net8.0/TallerLINQ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TallerLINQ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdba6b8b110b0612ac4cd35101c91229a59845e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6c284afe02936645d574425450cb4c2dddbb387")]
 [assembly: System.Reflection.AssemblyProductAttribute("TallerLINQ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TallerLINQ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
